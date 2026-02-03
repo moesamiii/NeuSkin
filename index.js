eda217279c27792da1ed2b5f0c69ecc8f6455e47
moesamiii <mohammedsamiii900@gmail.com>
2026-02-03 16:41:31 +0300

    updated

--- index.js
+++ index.js
@@ -33,10 +33,11 @@ loadClinicSettings();
 // ==============================
 // 📸 DOCTOR DATA
 // ==============================
+
 const DOCTOR_IMAGES = [
-  "https://drive.google.com/uc?export=view&id=1aHoA2ks39qeuMk9WMZOdotOod-agEonm",
-  "https://drive.google.com/uc?export=view&id=1Oe2UG2Gas6UY0ORxXtUYvTJeJZ8Br2_R",
-  "https://drive.google.com/uc?export=view&id=1_4eDWRuVme3YaLLoeFP_10LYHZyHyjUT",
+  "https://drive.google.com/file/d/1ibiePCccQytufxR6MREHQsuQcdKEgnHu/view?usp=sharing",
+  "https://drive.google.com/file/d/1oLw96zy3aWwJaOx6mwtZV173B7s5Rb64/view?usp=sharing",
+  "https://drive.google.com/file/d/1UkAzSHARtI-t-T_PCiY4RKcsxtkxR4Jf/view?usp=sharing",
 ];
 
 const DOCTOR_INFO = [
